4d8a3feff64269aa800033ac558c12d5563e9334
rayanulhassan <rayanulhassan@outlook.com>
2024-09-05 22:49:25 +0500

    main screen ui added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, computed, signal } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroAdjustmentsHorizontalSolid } from '@ng-icons/heroicons/solid';
+import { heroInformationCircle } from '@ng-icons/heroicons/outline';
+import {
+  phosphorFloppyDisk,
+  phosphorArrowCounterClockwise,
+} from '@ng-icons/phosphor-icons/regular';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent, DecimalPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [
+    provideIcons({
+      heroAdjustmentsHorizontalSolid,
+      phosphorFloppyDisk,
+      heroInformationCircle,
+      phosphorArrowCounterClockwise,
+    }),
+  ],
 })
 export class AppComponent {
   title = 'bingetap';
+  isDarkMode = signal<boolean>(false);
+
+  iconColor = computed(() => {
+    return this.isDarkMode() ? '#F5F4F4' : '#333333';
+  });
+
+  constructor() {
+    afterNextRender(() => {
+      if (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      )
+        this.isDarkMode.set(true);
+      else this.isDarkMode.set(false);
+
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', (event) => {
+          if (event.matches) this.isDarkMode.set(true);
+          else this.isDarkMode.set(false);
+        });
+    });
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+      }
+    },
   },
   plugins: [],
 }
